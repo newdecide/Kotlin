77abcf9b460dd76310fd2b3cfd4ebe4080b1aded
odongju <liberty-1@nate.com>
2019-01-11 16:55:38 +0900

    Gallary

---
+++ MyGallery/app/src/main/java/com/example/mygallery/MyPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.mygallery
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
+    // 뷰페이저가 표시할 프레그먼트 목록
+    private val items = ArrayList<Fragment>()
+
+    // position 위치의 프래그먼트
+    override fun getItem(p0: Int): Fragment {
+        return items[p0]
+    }
+
+    // 아이템의 갯수
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    // 아이템 갱신
+    fun updateFragments(items : List<Fragment>){
+        this.items.addAll(items)
+    }
+
+}
