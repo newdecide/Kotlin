bbc962f86dde31de3a9dd8110a1525e118e88f62
odongju <liberty-1@nate.com>
2019-01-14 19:51:39 +0900

    Xylophone

---
+++ Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.xylophone
+
+import android.content.pm.ActivityInfo
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val soundPool = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        SoundPool.Builder().setMaxStreams(8).build()
+    } else {
+        SoundPool(8, AudioManager.STREAM_MUSIC, 0)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // 화면이 가로 모드로 고정되게 하기
+//        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sounds.forEach{tune(it)}
+
+    }
+
+    private val sounds = listOf(
+        Pair(R.id.do1, R.raw.do1),
+        Pair(R.id.re, R.raw.re),
+        Pair(R.id.mi, R.raw.mi),
+        Pair(R.id.fa, R.raw.fa),
+        Pair(R.id.sol, R.raw.sol),
+        Pair(R.id.la, R.raw.la),
+        Pair(R.id.si, R.raw.si),
+        Pair(R.id.do2, R.raw.do2)
+
+    )
+
+    private fun tune(pitch: Pair<Int, Int>){
+        val soundId = soundPool.load(this, pitch.second, 1)
+        findViewById<TextView>(pitch.first).setOnClickListener {
+            soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+    }
+}
