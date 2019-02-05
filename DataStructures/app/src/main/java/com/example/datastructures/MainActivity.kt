4719a3424363f79a2cbe322d067b15621f158f2c
odongju <liberty-1@nate.com>
2019-02-05 20:55:06 +0900

    Stack(2)

--- DataStructures/app/src/main/java/com/example/datastructures/MainActivity.kt
+++ DataStructures/app/src/main/java/com/example/datastructures/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.datastructures
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Exception
+import java.util.jar.JarEntry
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,38 +14,104 @@ class MainActivity : AppCompatActivity() {
         var stacklist = StackWithList()
         println("is Stack empty : " + stacklist.isStackEmpty())
         stacklist.push("karthiq")
-        println("peek is : " +stacklist.peek())
+        println("peek is : " + stacklist.peek())
         stacklist.push(false)
         stacklist.push(5)
         stacklist.push(12.22)
         println("pop elements is : " + stacklist.pop())
         println("size is : " + stacklist.size())
         println("stack empty : " + stacklist.isStackEmpty())
+
+
+        val stack = StackUsingArray(5)
+        stack.push(1)
+        stack.push(7)
+        stack.push(5)
+        stack.push(3)
+        stack.push(2)
+        println("is stack empty : "+stack.isStackEmpty())
+        println("size of stack : " +stack.size())
+        println("peek : " + stack.peek())
+        println("pop : "+ stack.pop())
+        println("size of stack : " +stack.size())
+
+
     }
-}
 
-class StackWithList{
-    val elements: MutableList<Any> = mutableListOf()
+    class StackWithList {
+        val elements: MutableList<Any> = mutableListOf()
 
-    fun isEmpty() = elements.isEmpty()
+        fun isEmpty() = elements.isEmpty()
 
-    // 목록 요소가 있는지 확인
-    fun isStackEmpty() = elements.isEmpty()
-    // 크기
-    fun size() = elements.size
-    // 푸시
-    fun push(item: Any) = elements.add(item)
-    // 팝
-    fun pop(): Any? {
-        val item= elements.lastOrNull()
-        if(!isEmpty()){
-            elements.removeAt(elements.size -1)
+        // 목록 요소가 있는지 확인
+        fun isStackEmpty() = elements.isEmpty()
+
+        // 크기
+        fun size() = elements.size
+
+        // 푸시
+        fun push(item: Any) = elements.add(item)
+
+        // 팝
+        fun pop(): Any? {
+            val item = elements.lastOrNull()
+            if (!isEmpty()) {
+                elements.removeAt(elements.size - 1)
+            }
+            return item
         }
-        return item
+
+        // 엿보기
+        fun peek(): Any? = elements.lastOrNull()
+
+        override fun toString(): String = elements.toString()
     }
 
-    // 엿보기
-    fun peek() : Any? = elements.lastOrNull()
+    class StackUsingArray(size: Int) {
+        private var stackSize = size
+        private var stackArr = IntArray(stackSize)
+        private var top = -1
 
-    override fun toString(): String = elements.toString()
+        fun push(entry: Int) {
+            if (this.isStackFull()) {
+                println("Stack is full. Increasing the capacity.")
+                this.increaseStackCapacity()
+            }
+
+            top += 1
+            println("Adding: $entry")
+            this.stackArr[top] = entry
+        }
+
+        fun pop(): Any {
+            if(this.isStackEmpty()){
+                throw Exception("Stack is empty. Can not remove element.")
+            }
+            var entry = this.stackArr[top--]
+            println("Removed entry: $entry")
+            return entry
+        }
+        fun size(): Any{
+            return top+1
+        }
+        fun peek(): Any{
+            return stackArr[top]
+        }
+
+        private fun increaseStackCapacity() {
+            val newStack = IntArray(this.stackSize * 2)
+            for(i in 0 until stackSize){
+                newStack[i] = this.stackArr[i]
+            }
+            this.stackArr = newStack
+            this.stackSize = this.stackSize * 2
+        }
+
+        fun isStackEmpty(): Boolean {
+            return top === -1
+        }
+        private fun isStackFull(): Boolean{
+            return top === stackSize -1
+        }
+    }
 }
