da3324f8ecf95a86351f6f02a5a8b4850339b6b2
odongju <liberty-1@nate.com>
2019-02-06 22:33:49 +0900

    Queue(1)

--- DataStructures/app/src/main/java/com/example/datastructures/MainActivity.kt
+++ DataStructures/app/src/main/java/com/example/datastructures/MainActivity.kt
@@ -11,30 +11,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var stacklist = StackWithList()
-        println("is Stack empty : " + stacklist.isStackEmpty())
-        stacklist.push("karthiq")
-        println("peek is : " + stacklist.peek())
-        stacklist.push(false)
-        stacklist.push(5)
-        stacklist.push(12.22)
-        println("pop elements is : " + stacklist.pop())
-        println("size is : " + stacklist.size())
-        println("stack empty : " + stacklist.isStackEmpty())
+//        var stacklist = StackWithList()
+//        println("is Stack empty : " + stacklist.isStackEmpty())
+//        stacklist.push("karthiq")
+//        println("peek is : " + stacklist.peek())
+//        stacklist.push(false)
+//        stacklist.push(5)
+//        stacklist.push(12.22)
+//        println("pop elements is : " + stacklist.pop())
+//        println("size is : " + stacklist.size())
+//        println("stack empty : " + stacklist.isStackEmpty())
+//
+//
+//        val stack = StackUsingArray(5)
+//        stack.push(1)
+//        stack.push(7)
+//        stack.push(5)
+//        stack.push(3)
+//        stack.push(2)
+//        println("is stack empty : "+stack.isStackEmpty())
+//        println("size of stack : " +stack.size())
+//        println("peek : " + stack.peek())
+//        println("pop : "+ stack.pop())
+//        println("size of stack : " +stack.size())
 
+        var a = Queue(mutableListOf("karthiq", 2, 3, "four"))
 
-        val stack = StackUsingArray(5)
-        stack.push(1)
-        stack.push(7)
-        stack.push(5)
-        stack.push(3)
-        stack.push(2)
-        println("is stack empty : "+stack.isStackEmpty())
-        println("size of stack : " +stack.size())
-        println("peek : " + stack.peek())
-        println("pop : "+ stack.pop())
-        println("size of stack : " +stack.size())
+        // queue 7 추가
+        println(a.enqueue(7))
+        println(a) // print all
 
+        // queue 제거
+        println(a.dequeue())
+        println(a)
+
+        // queue 나갈 수 있는거 엿보기
+        println(a.peek())
+        println(a)
+
+        // queue 다음 대상 확인
+        print(a.size())
+
+        // 비어있는지 확인
+        println(a.isEmpty())
 
     }
 
@@ -84,23 +103,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         fun pop(): Any {
-            if(this.isStackEmpty()){
+            if (this.isStackEmpty()) {
                 throw Exception("Stack is empty. Can not remove element.")
             }
             var entry = this.stackArr[top--]
             println("Removed entry: $entry")
             return entry
         }
-        fun size(): Any{
-            return top+1
+
+        fun size(): Any {
+            return top + 1
         }
-        fun peek(): Any{
+
+        fun peek(): Any {
             return stackArr[top]
         }
 
         private fun increaseStackCapacity() {
             val newStack = IntArray(this.stackSize * 2)
-            for(i in 0 until stackSize){
+            for (i in 0 until stackSize) {
                 newStack[i] = this.stackArr[i]
             }
             this.stackArr = newStack
@@ -110,8 +131,31 @@ class MainActivity : AppCompatActivity() {
         fun isStackEmpty(): Boolean {
             return top === -1
         }
-        private fun isStackFull(): Boolean{
-            return top === stackSize -1
+
+        private fun isStackFull(): Boolean {
+            return top === stackSize - 1
         }
+    }
+}
+
+class Queue(list: MutableList<Any>) {
+    var items: MutableList<Any> = list
+    fun isEmpty(): Boolean = items.isEmpty()
+    fun size(): Int = items.count()
+    override fun toString() = items.toString()
+    fun enqueue(element: Any) {
+        items.add(element)
+    }
+
+    fun dequeue(): Any? {
+        if (this.isEmpty()) {
+            return null
+        } else {
+            return items.removeAt(0)
+        }
+    }
+
+    fun peek(): Any? {
+        return items[0]
     }
 }
