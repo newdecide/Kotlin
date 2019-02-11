3adb38efb09353262c64bd05bd315d7a5cda8c93
odongju <liberty-1@nate.com>
2019-02-11 14:34:51 +0900

    FirstTest

--- BasicStep1/app/src/main/java/com/example/basicstep1/MainActivity.kt
+++ BasicStep1/app/src/main/java/com/example/basicstep1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.basicstep1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basicstep1.step1.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // FirstTest 클래스
         // :: 메서드 불러올 때 사용
         doTest(FirstTest(::WriteLn))
+//        doTest(NumberTest(::WriteLn))
+//        doTest(StringTest(::WriteLn))
+//        doTest(AnyTest(::WriteLn))
+//        doTest(FunctionTest(::WriteLn))
     }
 
     private fun doTest(o : TestClass){
@@ -23,5 +27,4 @@ class MainActivity : AppCompatActivity() {
         // 코틀린은 findviewById 없이 바로 사용할 수 있다.
         txtMessage.text = "${txtMessage.text}${any.toString()}\n"
     }
-
 }
---
+++ BasicStep1/app/src/main/java/com/example/basicstep1/step1/FirstTest.kt
@@ -0,0 +1,35 @@
+package com.example.basicstep1.step1
+
+import com.example.basicstep1.TestClass
+
+// psvm 치면 자동입력 public static void main(앞글자 딴거)
+//fun main(args: Array<String>) {
+//
+//}
+
+
+// 변수 Any 파라미터 받는다.
+// ->
+// Unit 값이 없음을 정의하는 형
+class FirstTest (p : (Any) ->Unit): TestClass(p){
+    // controll + o = Override
+    override fun doTest() {
+        println("종결자(;) 없어도 됩니다.")
+        println("var는 읽기 쓰기, val 읽기 전용")
+
+        // 0만 입력하면 Int형으로 자동 생성
+        var nCount : Int = 0
+        // String 형 생략
+        var name = "psw"
+
+        val age = 0
+        // age ++ 안됨
+
+        var MyMoney : Int? = null
+//         println (message)
+        var message = "위에서는 엑세스 불가함"
+        println(hiMessage)
+        println("")
+    }
+    val hiMessage : String = "Hi"
+}
