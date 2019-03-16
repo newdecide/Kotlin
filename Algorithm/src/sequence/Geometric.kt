63a309762ae874ba635ac31b01791453b6b6bbcb
newdecide <loyalty0807@gmail.com>
2019-03-16 20:16:22 +0900

    "Fivonacci Sequence"

--- Algorithm/src/sequence/Geometric.kt
+++ Algorithm/src/sequence/Geometric.kt
@@ -8,8 +8,8 @@ fun main(args: Array<String>) {
     var R :Long = 3 // 수열의 공비
     var S :Long = A // 1수열의 합보관 변수
     var N :Long = 2 // 등비수열의 항순서, 2부터 시작
-    var i = 1
-    var j = 10
+    var i = 1 // 1씩 증가하는 변수
+    var j = 10 // 10번째가 되면 줄바꿔주는 항
     print("${S} ") // 등비수열 첫항
     while (true) {
             i++
