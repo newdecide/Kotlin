00198bee0443996aed85bd6ef18a1d18abe386df
newdecide <liberty-1@nate.com>
2019-04-06 23:26:45 +0900

    ColumnMajor

---
+++ Algorithm/src/arrangement/01_ColumnMajor.kt
@@ -0,0 +1,24 @@
+package arrangement
+// 열고정 배열
+fun main(args: Array<String>) {
+    var arr = Array(5, {IntArray(5)})
+    var v = 0 // 배열 원소에 저장되는 값
+    var c = 0 // 1열
+    var r = 0 // 1행
+
+    for(c in 0..4){ // 입력 반복문
+        for(r in 0..4){
+            v++
+            arr[r][c] = v
+        }
+    }
+    for(c in 0..4){ // 배열 출력 반복문
+        for(r in 0..4){
+            print("${arr[c][r]}\t")
+            if(r == 4){
+                println()
+            }
+        }
+
+    }
+}
